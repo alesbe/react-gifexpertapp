ee83d6c1feee7bfb1174ac0231ab022a6d93ed15
Nexobeta28 <alvaroesparza2@gmail.com>
2020-08-11 12:44:36 +0200

    Added docs (build)

---
+++ docs/precache-manifest.2087517208c85cbb51b0f6e05fc373fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d768b145e4cfca5440b92a7b2435bb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7341171c3e75ecb742c7",
+    "url": "/static/css/main.459067cf.chunk.css"
+  },
+  {
+    "revision": "92a5aaf936af7a3a2930",
+    "url": "/static/js/2.7c8d2b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c8d2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7341171c3e75ecb742c7",
+    "url": "/static/js/main.489eb2e0.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
